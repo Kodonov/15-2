178ec969a08b6c9094d195ad10e90c12a97833a9
Kodonov <93920158+Kodonov@users.noreply.github.com>
2022-06-08 07:34:51 -0500

    lesson1

---
+++ greeting/greeting.js
@@ -0,0 +1,26 @@
+// const str = prompt("message: 'Как вас зовут?'")
+// console.log("Welcome " + str)
+
+// const arr = ["первый элемент", "второй элемент", "последний элемент"];
+// console.log(arr[0]); 	// 'первый элемент'
+// console.log(arr[1]); 	// 'второй элемент'
+// console.log(arr[2]); 	// 'последний элемент'
+// console.log(arr[arr.length - 1]); // 'последний элемент
+
+const a = prompt("message: 'Выберите вет светафора")
+
+switch (a) {
+    case "зеленый":
+        console.log("Можно ехать");
+        break;
+    case "красный":
+         console.log("Запрещено ехать!")
+         break;
+    case "желтый":
+        console.log("Движение запрещено!")
+        break;
+    default:
+        console.log("Такого цвета светафора не существует!!1")    
+}
+const arr3 = [[[[[[[[[[[[[[[5]]]]]]]]]]]]]]]
+console.log(arr3[0][0][0][0][0][0][0][0][0][0][0][0][0][0][0])
